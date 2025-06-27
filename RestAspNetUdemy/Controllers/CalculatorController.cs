6439cda0136bfb4fb0db8895e0a2c4cf1fd42dad
LucasCNS <lucas.cns@hotmail.com>
2025-06-27 18:44:51 -0300

    feat: adds sum endpoint with string validation and conversion following SOLID and CleanCode principles.

---
+++ RestAspNetUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAspNetUdemy.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class CalculatorController : ControllerBase
+	{
+		private readonly ILogger<CalculatorController> _logger;
+
+		public CalculatorController(ILogger<CalculatorController> logger)
+		{
+			_logger = logger;
+
+		}
+
+		[HttpGet("sum/{firstNumber}/{secondNumber}")]
+		public IActionResult Sum(string firstNumber, string secondNumber)
+		{
+			if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber))
+			{
+				return BadRequest("One or both inputs are not valid numbers.");
+			}
+
+			decimal firstConvertedNumber = ConvertToNumeric(firstNumber);
+			decimal secondConvertedNumber = ConvertToNumeric(secondNumber);
+
+			decimal sum = firstConvertedNumber + secondConvertedNumber;
+
+			return Ok(sum);
+		}
+
+		private bool IsNumeric(string input)
+		{
+			bool isNumber = decimal.TryParse(input, out _);
+
+			return isNumber;
+		}
+
+		private decimal ConvertToNumeric(string input)
+		{
+			return Convert.ToDecimal(input);
+		}
+	}
+}
