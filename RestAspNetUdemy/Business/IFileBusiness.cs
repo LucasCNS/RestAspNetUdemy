21f7958ee7b2935b1d265ee56c5fb8ef52a0115a
LucasCNS <lucas.cns@hotmail.com>
2025-10-30 15:27:32 -0300

    feat: Adds FileDetailVO, FileBusiness and IFileBusiness

---
+++ RestAspNetUdemy/Business/IFileBusiness.cs
@@ -0,0 +1,12 @@
+﻿using RestAspNetUdemy.Data.VO;
+
+namespace RestAspNetUdemy.Business
+{
+	public interface IFileBusiness
+	{
+		public byte[] GetFile(string filename);
+		public Task<FileDetailVO> SaveFileToDisk(IFormFile file);
+		public Task<List<FileDetailVO>> SaveFilesToDisk(IFormFile file);
+
+	}
+}
---
+++ RestAspNetUdemy/Business/Implementations/FileBusiness.cs
@@ -0,0 +1,31 @@
+﻿using RestAspNetUdemy.Data.VO;
+
+namespace RestAspNetUdemy.Business.Implementations
+{
+	public class FileBusiness : IFileBusiness
+	{
+		private readonly string _basePath;
+		private readonly IHttpContextAccessor _context;
+
+		public FileBusiness(IHttpContextAccessor context)
+		{
+			_context = context;
+			_basePath = Directory.GetCurrentDirectory() + "\\UploadDir\\";
+		}
+
+		public byte[] GetFile(string filename)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<FileDetailVO>> SaveFilesToDisk(IFormFile file)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
